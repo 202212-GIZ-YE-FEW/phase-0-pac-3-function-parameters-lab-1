10893d00d287b5a6114d5f3b4a118e79ded427ff
san <sansamaa0@gmail.com>
2023-02-23 01:00:06 +0300

    h.w

---
+++ index.js
@@ -0,0 +1,9 @@
+function logTwoValues(value1, value2) {
+  console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function introductionWithLanguage(name, language = "Js") {
+  console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+// introductionWithLanguage();
